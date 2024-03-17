58d0b7128b0bf617a3f970c3d2bf4ce4ebac6dc0
Paul Felkner <paul.felkner@elunic.com>
2024-03-17 19:42:31 +0100

    remove undesired inputs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import SignIn from "./components/signin/SignIn";
